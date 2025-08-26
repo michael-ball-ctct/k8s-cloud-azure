43a3850af24e6e3f9921d9190bbdda67a9761f44
Michael Ball <michael_ball@trimble.com>
2025-08-27 11:57:48 +1200

    comment out all the stuff

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
-output "resource_group" {
-  value = local.rg_name
-}
+# output "resource_group" {
+#   value = local.rg_name
+# }
 
-output "resource_group_location" {
-  value = local.rg_loc
-}
+# output "resource_group_location" {
+#   value = local.rg_loc
+# }
 
-output "aks_name" {
-  value = azurerm_kubernetes_cluster.aks.name
-}
+# output "aks_name" {
+#   value = azurerm_kubernetes_cluster.aks.name
+# }
 
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
-  sensitive = true
-}
+# output "kube_config" {
+#   value     = azurerm_kubernetes_cluster.aks.kube_admin_config_raw
+#   sensitive = true
+# }
