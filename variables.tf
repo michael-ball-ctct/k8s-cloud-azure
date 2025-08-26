ac1ea7d7fea62eba18160447e9bb3ba3fbca73ff
Michael Ball <michael_ball@trimble.com>
2025-08-27 11:59:13 +1200

    re-enable all the things

--- variables.tf
+++ variables.tf
@@ -1,89 +1,89 @@
-# variable "suffix" {
-#   description = "mball"
-#   type        = number
-#   default     = 2
-# }
+variable "suffix" {
+  description = "mball"
+  type        = number
+  default     = 2
+}
 
-# variable "resource_group_name" {
-# 	description = "Resource group name"
-# 	type        = string
-# 	default     = "rg-trimble-004"
-# }
+variable "resource_group_name" {
+	description = "Resource group name"
+	type        = string
+	default     = "rg-trimble-004"
+}
 
-# variable "create_rg" {
-#   description = "Create the RG (true) or reference an existing one (false)"
-#   type        = bool
-#   default     = true
-# }
+variable "create_rg" {
+  description = "Create the RG (true) or reference an existing one (false)"
+  type        = bool
+  default     = true
+}
 
-# variable "prefix" {
-# 	description = "Name prefix"
-# 	type        = string
-# 	default     = "aks"
-# }
+variable "prefix" {
+	description = "Name prefix"
+	type        = string
+	default     = "aks"
+}
 
-# variable "aks_name" {
-# 	description = "AKS name"
-# 	type        = string
-# 	default     = "aks-cluster-004"
-# }
+variable "aks_name" {
+	description = "AKS name"
+	type        = string
+	default     = "aks-cluster-004"
+}
 
-# variable "vnet_address_space" {
-# 	description = "VNet CIDR"
-# 	type        = string
-# 	default     = "10.74.4.0/24"
-# }
+variable "vnet_address_space" {
+	description = "VNet CIDR"
+	type        = string
+	default     = "10.74.4.0/24"
+}
 
-# variable "subnet_address_prefix" {
-#   description = "Subnet CIDR"
-#   type        = string
-#   default     = "10.74.4.0/26"
-# }
+variable "subnet_address_prefix" {
+  description = "Subnet CIDR"
+  type        = string
+  default     = "10.74.4.0/26"
+}
 
-# variable "dns_service_ip" {
-# 	description = "DNS service IP for AKS"
-# 	type        = string
-# 	default     = "10.74.4.33"
-# }
+variable "dns_service_ip" {
+	description = "DNS service IP for AKS"
+	type        = string
+	default     = "10.74.4.33"
+}
 
-# variable "service_cidr" {
-# 	description = "Service CIDR for AKS"
-# 	type        = string
-# 	default     = "10.74.4.32/26"
-# }
+variable "service_cidr" {
+	description = "Service CIDR for AKS"
+	type        = string
+	default     = "10.74.4.32/26"
+}
 
-# variable "nodepool_name" {
-# 	description = "Node pool name"
-# 	type        = string
-# 	default     = "npapps004"
-# }
+variable "nodepool_name" {
+	description = "Node pool name"
+	type        = string
+	default     = "npapps004"
+}
 
-# variable "node_vm_size" {
-# 	description = "VM size"
-# 	type        = string
-# 	default     = "Standard_D2as_v5"
-# }
+variable "node_vm_size" {
+	description = "VM size"
+	type        = string
+	default     = "Standard_D2as_v5"
+}
 
-# variable "node_count" {
-# 	description = "Initial node count"
-# 	type        = number
-# 	default     = 2
-# }
+variable "node_count" {
+	description = "Initial node count"
+	type        = number
+	default     = 2
+}
 
-# variable "kubernetes_version" {
-# 	description = "AKS version (optional)"
-# 	type        = string
-# 	default     = ""
-# }
+variable "kubernetes_version" {
+	description = "AKS version (optional)"
+	type        = string
+	default     = ""
+}
 
-# variable "location" {
-# 	description = "Azure region"
-# 	type        = string
-# 	default     = "australiaeast"
-# }
+variable "location" {
+	description = "Azure region"
+	type        = string
+	default     = "australiaeast"
+}
 
-# locals {
-#   full_prefix = "${var.prefix}${var.suffix}"
-# }
+locals {
+  full_prefix = "${var.prefix}${var.suffix}"
+}
 
 
